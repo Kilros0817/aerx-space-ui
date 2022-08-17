fa4a7e98d96ba9cc640243cda3764f9add1e44c5
username <user email>
2022-08-17 21:43:52 +0800

    Component clash resolved

--- components/LandingPage/sectionOne.tsx
+++ components/LandingPage/sectionOne.tsx
@@ -1,4 +1,3 @@
-//test
 import React from 'react'
 import {
     Box,
@@ -6,7 +5,8 @@ import {
     Image,
     Text,
     Heading,
-    Container
+    Container,
+    Flex
 } from "@chakra-ui/react";
 import WithDots from "./WithDots";
 import { useDispatch, useSelector } from '../../store/store';
@@ -19,57 +19,62 @@ const SectionOne: React.FC = () => {
     const { group2, group3, handWriting, star } = useSelector(getUserState);
     return (
         <Box>
-            <Container maxWidth="container.xlg" >
-                <WithDots>
-                    <Box display='flex' alignItems="center" my="20" flexDirection="row" marginRight='95px' marginLeft="95px">
-                        <Box mr={0} width={585}>
-                            <Image height='237px' src={group2} position='absolute' className='group1-b' />
-                            <Image height='237px' src={group3} position='absolute' className='group2-b' />
-                            <Image height='237px' src={handWriting} />
-                        </Box>
-                        <Box width={739} className='monetize' marginLeft="125px">
-                            <Heading fontSize='6xl' color='#322E65'>
-                                Monetize your ideas
-                            </Heading>
-                            <Box display="flex" flexDirection='row'>
-                                <Image src={star} mr={2} />
-                                <Text
-                                    className='easily'
-                                >
-                                    Easily
-                                </Text>
-                            </Box>
-                            <Text
-                                fontSize='2xl'
-                                fontStyle='normal'
-                                lineHeight={1.5}
-                                fontWeight='300'
-                                justifyContent='left'
-                                marginRight={13}
-                                mt={35}
-                                color="#322E6580"
-                            >
-                                In Aerx, makers can easily monetize content and retain their ownership rights.
-                            </Text>
-                            <Text
-                                fontSize='2xl'
-                                fontStyle='normal'
-                                lineHeight={1.5}
-                                fontWeight='300'
-                                justifyContent='left'
-                                marginRight={13}
-                                mt={35}
-                                color="#322E6580"
-                            >
-                                Just publish what you have created: text, video, picture or audio. It doesn't matter if you are a professional artist who just came up with a funny meme, an aspiring musician or an author of interesting texts. Post it and get rewarded if other users like it.
+            {/* <Container maxWidth="container.xlg"  > */}
+            <WithDots>
+                <Flex marginTop="239px"
+                    bgImage="url('../Group 5392.png')"
+                    bgRepeat="no-repeat"
+                    bgPosition="center left"
+                    bgSize="80%"
+                    gap='51px'
 
-                            </Text>
-                        </Box>
+                    height="100vh"
+                >
+                    <Box mr={0} width={585}>
                     </Box>
-                </WithDots>
-            </Container >
-        </Box>
+                    <Box width={739} className='monetize' marginLeft="125px">
+                        <Heading fontStyle="italic"
+                            fontSize="20px"
+                            lineHeight="30px"
+                            fontWeight='300'
+                            mt="32px"
+                            color="#322E6580">
+                            Monetize your ideas
+                        </Heading>
+                        <Box display="flex" flexDirection='row'>
+                            <Image src={star} mr={2} />
+                            <Text
+                                className='easily' fontWeight="800" fontSize="96px" lineHeight="114px" fontFamily="Poppins" color="#8d00ff" fontStyle="italic" letterSpacing="-0.03em"
+                            >
+                                Easily
+                            </Text>
+                        </Box>
+                        <Text
+                            fontSize='20px'
+                            lineHeight="30px"
+                            fontWeight='300'
+                            //  justifyContent='left'
 
+                            mt="48px"
+                            color="#322E6580"
+                        >
+                            In Aerx, makers can easily monetize content and retain their ownership rights.<br />
+                        </Text>
+                        <Text
+                            fontSize='20px'
+                            lineHeight="30px"
+                            fontWeight='300'
+
+                            color="#322E6580"
+                        >
+                            Just publish what you have created: text, video, picture or audio. It doesn't matter if you are a professional artist who just came up with a funny meme, an aspiring musician or an author of interesting texts. Post it and get rewarded if other users like it.
+
+                        </Text>
+                    </Box>
+                </Flex>
+            </WithDots>
+            {/* </Container > */}
+        </Box>
 
 
     )
