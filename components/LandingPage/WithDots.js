86c73adad43abcfb5a9fa8d3fc9c63ccf086c0fc
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2022-08-19 10:49:17 +0200

    chore: resolve conflicts

--- components/LandingPage/WithDots.js
+++ components/LandingPage/WithDots.js
@@ -1,58 +1,59 @@
 import { Box } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
+import CustomImage from "./CustomImage";
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-const colors = ["#FFA2F6", "#7E4CF9", "#A0FFCC"];
+const StarComponent = ({ top, left, radius }) => (
+    <Box position="absolute" top={top} left={left}>
+        <img
+            src="/resources/star.svg"
+            style={{
+                width: radius,
+                height: radius,
+            }}
+        />
+    </Box>
+);
 
-const toPercent = (num) => num + "%";
-
-const WithDots = ({ children }) => {
+const WithStar = ({ children }) => {
     const positions = {
         top: [
             ["30%", "70%"],
             ["60%", "10%"],
-            ["100", "500"],
+            [100, 500],
         ],
         left: [30, ["90%", "50%"], ["95%"]],
     };
 
-    const [dots, setDots] = useState([]);
+    const [stars, setStars] = useState([]);
 
     useEffect(() => {
-        const dots = [];
-        for (let i = 0; i < getRandomArbitrary(3, 6); i++) {
-            dots.push({
-                top: toPercent(getRandomArbitrary(5, 100)),
-                left: toPercent(getRandomArbitrary(5, 100)),
+        const stars = [];
+        for (let i = 0; i < 6; i++) {
+            stars.push({
+                top: positions.top[i],
+                left: positions.left[i],
                 radius: getRandomArbitrary(10, 20),
-                color: colors[
-                    Math.floor(getRandomArbitrary(0, colors.length - 1))
-                ],
             });
         }
-        console.log(dots);
-        setDots(dots);
+        setStars(stars);
     }, []);
 
     return (
         <Box position="relative">
-            {dots.map((dot, i) => (
-                <Box
-                    position="absolute"
-                    key={dot + i}
-                    borderRadius="full"
-                    bg={dot.color}
-                    top={dot.top}
-                    left={dot.left}
-                    w={dot.radius}
-                    h={dot.radius}
+            {stars.map((star, i) => (
+                <StarComponent
+                    key={star + i}
+                    top={star.top}
+                    left={star.left}
+                    radius={star.radius}
                 />
             ))}
             {children}
         </Box>
     );
 };
-export default WithDots;
+export default WithStar;
--- components/LandingPage/WithStars.js
+++ components/LandingPage/WithStars.js
@@ -1,124 +1,58 @@
-<<<<<<< HEAD:components/LandingPage/WithStars.tsx
-import { Box } from "@chakra-ui/react";
-import Image from "next/image";
-import React, { useState, useEffect } from "react";
-import CustomImage from "./CustomImage";
-
-function getRandomArbitrary(min: number, max: number) {
-    return Math.random() * (max - min) + min;
-}
-
-const StarComponent: React.FC<{top: number, left: number, radius: number}> = ({ top, left, radius }) => (
-    <Box position="absolute" top={top} left={left}>
-        <Image
-            src="/star.svg"
-            width={radius}
-            height={radius}
-            style={{
-                width: radius,
-                height: radius,
-            }}
-        />
-    </Box>
-);
-
-const WithStar: React.FC<{children: unknown}> = ({ children }) => {
-    const positions = {
-        top: [
-            ["30%", "70%"],
-            ["60%", "10%"],
-            [100, 500],
-        ],
-        left: [30, ["90%", "50%"], ["95%"]],
-    };
-
-    const [stars, setStars] = useState<any>([]);
-
-    useEffect(() => {
-        const stars = [];
-        for (let i = 0; i < 6; i++) {
-            stars.push({
-                top: positions.top[i],
-                left: positions.left[i],
-                radius: getRandomArbitrary(10, 20),
-            });
-        }
-        setStars(stars);
-    }, []);
-
-    return (
-        <Box position="relative">
-            {stars.map((star: { top: number; left: number; radius: number; }, i: number) => (
-                <StarComponent
-                    key={i}
-                    top={star.top}
-                    left={star.left}
-                    radius={star.radius}
-                />
-            ))}
-            {children}
-        </Box>
-    );
-};
-export default WithStar;
-=======
 import { Box } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
-import CustomImage from "./CustomImage";
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-const StarComponent = ({ top, left, radius }) => (
-    <Box position="absolute" top={top} left={left}>
-        <img
-            src="/resources/star.svg"
-            style={{
-                width: radius,
-                height: radius,
-            }}
-        />
-    </Box>
-);
+const colors = ["#FFA2F6", "#7E4CF9", "#A0FFCC"];
 
-const WithStar = ({ children }) => {
+const toPercent = (num) => num + "%";
+
+const WithDots = ({ children }) => {
     const positions = {
         top: [
             ["30%", "70%"],
             ["60%", "10%"],
-            [100, 500],
+            ["100", "500"],
         ],
         left: [30, ["90%", "50%"], ["95%"]],
     };
 
-    const [stars, setStars] = useState([]);
+    const [dots, setDots] = useState([]);
 
     useEffect(() => {
-        const stars = [];
-        for (let i = 0; i < 6; i++) {
-            stars.push({
-                top: positions.top[i],
-                left: positions.left[i],
+        const dots = [];
+        for (let i = 0; i < getRandomArbitrary(3, 6); i++) {
+            dots.push({
+                top: toPercent(getRandomArbitrary(5, 100)),
+                left: toPercent(getRandomArbitrary(5, 100)),
                 radius: getRandomArbitrary(10, 20),
+                color: colors[
+                    Math.floor(getRandomArbitrary(0, colors.length - 1))
+                ],
             });
         }
-        setStars(stars);
+        console.log(dots);
+        setDots(dots);
     }, []);
 
     return (
         <Box position="relative">
-            {stars.map((star, i) => (
-                <StarComponent
-                    key={star + i}
-                    top={star.top}
-                    left={star.left}
-                    radius={star.radius}
+            {dots.map((dot, i) => (
+                <Box
+                    position="absolute"
+                    key={dot + i}
+                    borderRadius="full"
+                    bg={dot.color}
+                    top={dot.top}
+                    left={dot.left}
+                    w={dot.radius}
+                    h={dot.radius}
                 />
             ))}
             {children}
         </Box>
     );
 };
-export default WithStar;
->>>>>>> f33d6d9117d31cc5c3a372d75af89acb1f28857e:components/LandingPage/WithStars.js
+export default WithDots;
