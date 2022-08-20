900de3e8741c88070114917f19aa12682e51b09d
Mohzcrea8me <mohzcrea8me@gmail.com>
2022-08-21 07:43:31 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,24 +6,67 @@ import { ChakraProvider } from "@chakra-ui/react";
 import myTheme from "../lib/theme.js";
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
+import { useState, useEffect } from 'react';
+import { fetchpostsData } from '../hooks/useFetchPost';
+import { getBalance } from '../lib/aexContract';
+import { initNearConnection, checkProfile } from '../lib/auth';
+import { nearStore } from '../store/near';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  
+  const [isLoading, setIsLoading] = useState(true);
+  const nearState = nearStore((state) => state);
+  //1) Initialise near connection and contracts
+  useEffect(() => {
+    if (isLoading) {
+      initNearConnection(nearState);
+      //set isLoading to false
+      setIsLoading(false);
+    }
+  }, [isLoading]);
+
+  //Run checks on each contract to confirm they are succesfully loaded and at same time save needed informations to state
+  useEffect(() => {
+    //2) check profile and set profile to state(if user has registered) 
+    if (!isLoading) {
+      (async () => {
+        await checkProfile(nearState);
+      })();
+    }
+  }, [isLoading, nearState.accountId, nearState.pnftContract]);
+
+  useEffect(() => {
+    //3) get balance and set to state
+    if (!isLoading) {
+      (async () => {
+        await getBalance(nearState);
+      })();
+    }
+  }, [isLoading, nearState.accountId, nearState.tokenContract]);
+
+  useEffect(() => {
+    //fetch posts 
+    if (!isLoading) {
+      (async () => {
+        await fetchpostsData(nearState);
+      })();
+    }
+  }, [isLoading, nearState.accountId, nearState.pnftContract]);
+
+  //Todo: add more contracts functions and set state for all needed informations
+
+
   return (
-    
-    <div className="profile <div  className='w-[39%] h-[100vh] overflow-hidden'>">
     <Provider store={store}>
-    <ChakraProvider
-                theme={myTheme}
-            >
-                <ThemeProvider attribute="class">
-    <Component {...pageProps} />
-    </ThemeProvider>
-    </ChakraProvider>
+      <ChakraProvider
+        theme={myTheme}
+      >
+        <ThemeProvider attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ChakraProvider>
     </Provider>
-    </div>
-  ) 
+  )
 }
 
 export default MyApp
