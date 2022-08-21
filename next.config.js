72a78c8ee643d562b16020286cab5b6bd070cf59
Mohzcrea8me <mohzcrea8me@gmail.com>
2022-08-21 11:14:42 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  }
+}
 
 module.exports = nextConfig
