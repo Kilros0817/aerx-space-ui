0d2057c78617472a56f49725c415db938821b9f0
Mohzcrea8me <mohzcrea8me@gmail.com>
2022-08-23 09:31:36 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: true };
+//   webpack5: true,
+//   webpack: (config) => {
+//     config.resolve.fallback = { fs: true };
 
     return config;
   },
