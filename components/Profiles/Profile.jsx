339ce31350e9ed3809a60dccf6533fbcb32273c7
adedamolabasit <adedamolabasit09@gmail.com>
2022-09-20 10:30:56 +0100

    latest

--- components/Profiles/Profile.jsx
+++ components/Profiles/Profile.jsx
@@ -58,8 +58,8 @@ function Profile(props) {
       position="absolute"
       height="100%"
       w="257.56px"
-      bgImage="url('../resources/pd.svg') "
-      // bgImage={`url('${nearState.profile.profileImg}')`}
+      // bgImage="url('../resources/pd.svg') "
+      bgImage={`url('${nearState.profile.profileImg}')`}
       bgSize="100% 45%"
       bgRepeat="no-repeat"
       bgPosition="top"
@@ -124,8 +124,8 @@ function Profile(props) {
                 mt="21px"
                 justifyContent="center"
                 textTransform="capitalize"
-                // onMouseEnter={props.hoverClick }
-                // onMouseLeave={props.leaveClick }
+                onClick={props.circleClick}
+                
               >
                 {nearState.profile.fullName}
               </Heading>
