85d1ab9b56e34bc6670846dd86d038ef5bfaae5a
Mohzcrea8me <mohzcrea8me@gmail.com>
2022-12-23 19:10:10 +0800

    new contracts update

--- utils/constants/contract.ts
+++ utils/constants/contract.ts
@@ -1,7 +1,7 @@
 //constants
-export const TOKEN_CONTRACT_NAME = "aextestnew.mohzcrea8me.testnet"; //to load aex_token contract
-export const PROFILE_CONTRACT_NAME = "profile001.mohzcrea8me.testnet"; //to load profile_contract contract
-export const DEX_CONTRACT_NAME = "aeswaptestnew.mohzcrea8me.testnet"; //to load Dex contract
+export const TOKEN_CONTRACT_NAME = "aextokentest1.mohzcrea8me.testnet"; //to load aex_token contract
+export const PROFILE_CONTRACT_NAME = "aerxprofiletest1.mohzcrea8me.testnet"; //to load profile_contract contract
+export const DEX_CONTRACT_NAME = "aerxswaptest1.mohzcrea8me.testnet"; //to load Dex contract
 export const NFT_MARKETPLACE = "aemarket111.mohzcrea8me.testnet"; //to load Nft marketplace contract
 
 export const EXCHANGE_CURRENCY = ["NEAR", "AEX"] as const;
